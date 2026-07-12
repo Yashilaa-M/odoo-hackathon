08d00650749aa92892c0d1a158a51c96ff2d8cdf
Yashilaa-M <yashilaam@gmail.com>
2026-07-12 17:43:01 +0530

    chore: add .eslintrc.js to configure @typescript-eslint for backend

---
+++ odoo/t_ops/backend/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'],
+    tsconfigRootDir: __dirname,
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    // add or override rules here
+  },
+};
